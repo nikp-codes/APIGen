18232326e8745bf74639f8d9373797318ad5c0cc
nikhilmalavia <malavianikhil@gmail.com>
2025-08-21 13:33:33 +0530

    - Configure custom template to use @Validated and BindingResult - Add HttpServletRequest, HttpServletResponse to method by default

--- src/main/java/com/practice/gen/APIGen/controllers/AccountController.java
+++ src/main/java/com/practice/gen/APIGen/controllers/AccountController.java
@@ -2,7 +2,10 @@ package com.practice.gen.APIGen.controllers;
 
 import com.practice.gen.APIGen.controller.AccountApi;
 import com.practice.gen.APIGen.models.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,27 +17,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountController implements AccountApi {
 
     @Override
-    public ResponseEntity<Void> changePassword(ChangePasswordRequest changePasswordRequest) {
+    public ResponseEntity<Void> changePassword(ChangePasswordRequest changePasswordRequest, BindingResult errors, HttpServletRequest request, HttpServletResponse response) {
         return null;
     }
 
     @Override
-    public ResponseEntity<Success> loginUser(LoginUserRequest loginUserRequest) {
+    public ResponseEntity<Success> loginUser(LoginUserRequest loginUserRequest, BindingResult errors, HttpServletRequest request, HttpServletResponse response) {
         return null;
     }
 
     @Override
-    public ResponseEntity<RefreshToken200Response> refreshToken(RefreshTokenRequest refreshTokenRequest) {
+    public ResponseEntity<RefreshToken200Response> refreshToken(RefreshTokenRequest refreshTokenRequest, BindingResult errors, HttpServletRequest request, HttpServletResponse response) {
         return null;
     }
 
     @Override
-    public ResponseEntity<Success> registerUser(User user) {
+    public ResponseEntity<Success> registerUser(User user, BindingResult errors, HttpServletRequest request, HttpServletResponse response) {
         return null;
     }
 
     @Override
-    public ResponseEntity<Void> resetPassword(ResetPasswordRequest resetPasswordRequest) {
+    public ResponseEntity<Void> resetPassword(ResetPasswordRequest resetPasswordRequest, BindingResult errors, HttpServletRequest request, HttpServletResponse response) {
         return null;
     }
 }
--- src/main/java/com/practice/gen/APIGen/controllers/UsersController.java
+++ src/main/java/com/practice/gen/APIGen/controllers/UsersController.java
@@ -2,7 +2,10 @@ package com.practice.gen.APIGen.controllers;
 
 import com.practice.gen.APIGen.controller.UsersApi;
 import com.practice.gen.APIGen.models.User;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,27 +18,27 @@ import java.util.List;
 public class UsersController implements UsersApi {
 
     @Override
-    public ResponseEntity<User> createUser(User user) {
+    public ResponseEntity<User> createUser(User user, BindingResult errors, HttpServletRequest request, HttpServletResponse response) {
         return null;
     }
 
     @Override
-    public ResponseEntity<Void> deleteUserById(Integer id) {
+    public ResponseEntity<Void> deleteUserById(Integer id, HttpServletRequest request, HttpServletResponse response) {
         return null;
     }
 
     @Override
-    public ResponseEntity<List<User>> getAllUsers() {
+    public ResponseEntity<List<User>> getAllUsers(HttpServletRequest request, HttpServletResponse response) {
         return null;
     }
 
     @Override
-    public ResponseEntity<User> getUserById(Integer id) {
+    public ResponseEntity<User> getUserById(Integer id, HttpServletRequest request, HttpServletResponse response) {
         return null;
     }
 
     @Override
-    public ResponseEntity<Void> updateUserById(Integer id, User user) {
+    public ResponseEntity<Void> updateUserById(Integer id, User user, BindingResult errors, HttpServletRequest request, HttpServletResponse response) {
         return null;
     }
 }
